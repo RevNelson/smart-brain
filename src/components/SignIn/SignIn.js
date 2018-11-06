9b4fdd09043bb0e855e47d5c9d26a18944cf9d38
Michael Nelson <revnelson@gmail.com>
2018-11-07 01:30:49 +0800

    Merging with JWT

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import './SignIn.css';
-=======
-import React from "react";
->>>>>>> 40fcaa653cf46fd016c7d12de2ec39dad1a4e4dd
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -58,40 +54,6 @@ class SignIn extends React.Component {
       });
   };
 
-<<<<<<< HEAD
-    render() {
-        const { onRouteChange } = this.props;
-        return (
-            <article className="br3 ba b--gray dark-gray mv4 w-100 w-50-m w-25-l mw6 center">
-                <main className="pa4 gray">
-                    <div className="measure">
-                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input className="pa2 input-reset ba bg-transparent hover-black hover-bg-black hover-white w-100"
-                                       type="email" name="email-address" id="email-address" onChange={this.onEmailChange}/>
-                            </div>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba hover-black bg-transparent hover-bg-black hover-white w-100"
-                                       type="password" name="password" id="password" onChange={this.onPasswordChange}/>
-                            </div>
-                            <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
-                        </fieldset>
-                        <div className="">
-                            <button className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                                    type="submit" onClick={this.onSubmit}>Sign In</button>
-                        </div>
-                        <div className='lh-copy mt3'>
-                            <p className='f6 link dim gray db pointer' onClick={() => onRouteChange('register')}>Register</p>
-                        </div>
-                    </div>
-                </main>
-            </article>
-        )
-    }
-=======
   render() {
     const { onRouteChange } = this.props;
     return (
@@ -150,7 +112,6 @@ class SignIn extends React.Component {
       </article>
     );
   }
->>>>>>> 40fcaa653cf46fd016c7d12de2ec39dad1a4e4dd
 }
 
 export default SignIn;
